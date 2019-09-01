f895d31f67fed663f724f3c84cc4b43b6a5cbf71
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-09-01 13:27:19 +0530

    calculations done final commit

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,10 +1,15 @@
 package question1;
 
-import java.util.Scanner;*;
 public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        double s = 0.0;
+        for (int i = 1; i <= n; i++) {
+            s = s + (1 / i);
+        }
+        System.out.println(s);
+    }
 
     }
 }
