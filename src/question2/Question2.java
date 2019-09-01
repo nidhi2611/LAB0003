36dbeb9c48887622630eac656aac69161759cc6b
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-09-01 13:46:06 +0530

    final commit

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -6,6 +6,9 @@ public class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double m = sc.nextDouble();
+        int rs = 0;
+        rs = (int) m;
+        System.out.println(rs + "rupees and " + (int) ((m - rs) * 100) + " paisa");
 
 
     }
