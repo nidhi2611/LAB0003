0b4b858f9328fd4a0dd693160d5a1282d09fe581
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-09-01 13:34:41 +0530

    initial commit

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,15 +1,19 @@
 package question1;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         double s = 0.0;
+        double k = 0.0;
         for (int i = 1; i <= n; i++) {
-            s = s + (1 / i);
+            s = (1 / i);
+            k = k + s;
         }
-        System.out.println(s);
+        System.out.println(k);
     }
 
     }
-}
+
---
+++ src/question2/Question2.java
@@ -0,0 +1,9 @@
+package question2;
+
+public class Question2 {
+    pubblic
+
+    static void main(String[] args) {
+
+    }
+}
