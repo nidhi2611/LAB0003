e9814c9063c4704271a975c569b736529344e00d
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-09-01 13:24:40 +0530

    value of n inputted

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,6 +1,10 @@
 package question1;
 
+import java.util.Scanner;*;
 public class Question1 {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+    }
 }
